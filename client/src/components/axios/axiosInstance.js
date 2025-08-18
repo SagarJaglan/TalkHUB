131cbcdd37d1aabc8f0a3aa22d3847b4a56859e2
SJ <69107909+SagarJaglan@users.noreply.github.com>
2025-08-19 01:28:17 +0530

    Update axiosInstance.js

--- client/src/components/axios/axiosInstance.js
+++ client/src/components/axios/axiosInstance.js
@@ -4,9 +4,8 @@ const BASE_URL = import.meta.env.VITE_DB_URL;
 
 export const axiosInstance = axios.create({
   baseURL: BASE_URL,
-  withCredentials: true, // 🔑 sends cookies along with every request
+  withCredentials: true,
   headers: {
-    "Content-Type": "application/json",
-  },
+    "Content-Type": "application/json"
+  }
 });
-
